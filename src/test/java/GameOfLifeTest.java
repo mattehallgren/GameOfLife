5c12dfc0928f9a2f5ed9fc933a0f56376196087e
Mattias Hallgren <mattias@tada.se>
2018-12-10 09:21:18 +0100

    test 3 neighbors

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -34,7 +34,7 @@ public class GameOfLifeTest {
 
     @Test
     public void testGeneration2Neighbors() {
-        //Any live cell with two live neighbors lives on to the next original.
+        //Any live cell with two live neighbors lives on to the next generation.
         boolean[][] original = new boolean[5][5];
         original[1][0] = true;
         original[1][1] = true;
@@ -55,23 +55,21 @@ public class GameOfLifeTest {
 
     @Test
     public void testGeneration3Neighbors() {
-        //Any live cell with three live neighbors lives on to the next original.
+        //Any live cell with three live neighbors lives on to the next generation.
         boolean[][] original = new boolean[5][5];
-        original[1][0] = true;
+        original[0][2] = true;
         original[1][1] = true;
         original[1][2] = true;
         original[1][3] = true;
 
         boolean[][] expected = new boolean[5][5];
-        expected[0][0] = false;
-        expected[0][1] = false;
-        expected[0][2] = true;
-        expected[2][0] = false;
-        expected[2][1] = true;
-        expected[2][2] = true;
         expected[0][1] = true;
+        expected[0][2] = true;
+        expected[0][3] = true;
         expected[1][1] = true;
         expected[1][2] = true;
+        expected[1][3] = true;
+        expected[2][2] = true;
         Assert.assertEquals(new Generation(expected), new Generation(original).progress());
     }
 
@@ -87,10 +85,28 @@ public class GameOfLifeTest {
         expected[0][0] = false;
         expected[0][1] = false;
         expected[0][2] = false;
+        expected[0][3] = false;
+        expected[0][4] = false;
+        expected[1][0] = false;
+        expected[1][1] = false;
+        expected[1][2] = false;
+        expected[1][3] = false;
+        expected[1][4] = false;
         expected[2][0] = false;
         expected[2][1] = false;
         expected[2][2] = false;
-        expected[1][1] = false;
+        expected[2][3] = false;
+        expected[2][4] = false;
+        expected[3][0] = false;
+        expected[3][1] = false;
+        expected[3][2] = false;
+        expected[3][3] = false;
+        expected[3][4] = false;
+        expected[4][0] = false;
+        expected[4][1] = false;
+        expected[4][2] = false;
+        expected[4][3] = false;
+        expected[4][4] = false;
         Assert.assertEquals(new Generation(expected), new Generation(original).progress());
     }
 
